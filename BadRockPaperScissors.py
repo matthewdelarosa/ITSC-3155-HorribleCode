fa16d82f47035cfb1b49c46d6edecbdf7281838d
matthewdelarosa <mdelaro2@charlotte.edu>
2026-02-10 14:47:53 -0500

    Feat: Added functionality to calculate if you or the computer won

--- BadRockPaperScissors.py
+++ BadRockPaperScissors.py
@@ -14,4 +14,15 @@ def f2():
     v4 = "Paper"
   else:
     v4 = "Scissors"
-    
+
+def f3():
+    if v1 == v4:
+        print("It's a tie!")
+    elif v1 == "Rock" and v4 == "Scissors":
+        print("You win!")
+    elif v1 == "Paper" and v4 == "Rock":
+        print("You win!")
+    elif v1 == "Scissors" and v4 == "Paper":
+        print("You win!")
+    else:
+        print("You lose!")
