8f7bded68ff357759ab576023064c5f172702c50
matthewdelarosa <mdelaro2@charlotte.edu>
2026-02-10 14:45:40 -0500

    Feat: Added function to randomly generate Rock Paper or Scissors

--- BadRockPaperScissors.py
+++ BadRockPaperScissors.py
@@ -1,6 +1,17 @@
 
+import random
+
+
 def f1():
   print("Choose Rock, Paper, or Scissors: ")
   v1 = input()
 
-
+def f2():
+  v3 = random.randrange(1, 4, 1)
+  if v3 == 1:
+    v4 = "Rock"
+  elif v3 == 2:
+    v4 = "Paper"
+  else:
+    v4 = "Scissors"
+    
