17e76435456f68e08470b412822d19fe7b1b470a
matthewdelarosa <mdelaro2@charlotte.edu>
2026-02-10 14:51:39 -0500

    fix: added returns and variable inputs for the functions

--- BadRockPaperScissors.py
+++ BadRockPaperScissors.py
@@ -1,10 +1,10 @@
 
 import random
 
-
 def f1():
   print("Choose Rock, Paper, or Scissors: ")
   v1 = input()
+  return v1
 
 def f2():
   v3 = random.randrange(1, 4, 1)
@@ -14,8 +14,9 @@ def f2():
     v4 = "Paper"
   else:
     v4 = "Scissors"
+  return v4
 
-def f3():
+def f3(v1, v4):
     if v1 == v4:
         print("It's a tie!")
     elif v1 == "Rock" and v4 == "Scissors":
